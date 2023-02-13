1351d3be89cfd54ae169d9acf20ac18bb5b91866
atinside <simay.aksoz@st.uskudar.edu.tr>
2023-02-13 13:39:54 +0300

    düzenleme yapıldı

--- meetings/epa-unepsa-meetings.php
+++ meetings/epa-unepsa-meetings.php
@@ -37,34 +37,34 @@
               <p><b>April 2024</b></p>
               <a href="http://www.europaediatrics.org/" target="_blank">www.europaediatrics.org/</a>
               <p>11th Europaediatrics Congress<br>
-              17-21 April 2024, Antalya, Turkey</p>
+              17-21 April 2024, Antalya, Turkey</p><br>
 
               <p><b>October 2023</b></p>
               <a href="https://hteuropaediatrics.org/" target="_blank">www.hteuropaediatrics.org/</a>
               <p>1st Hot Topics Europaediatrics<br>
               Joint Meeting of EPA/UNEPSA and Pediatric Association of Serbia<br>
-              5-8 October 2023, Belgrade, Serbia</p>
+              5-8 October 2023, Belgrade, Serbia</p><br>
 
               
               <p><b>March 2023</b></p>
               <a href="https://www.ecpcp.eu/index.php?id=81&tx_news_pi1%5Bnews%5D=18&tx_news_pi1%5Bcontroller%5D=News&tx_news_pi1%5Baction%5D=detail&cHash=364da5e0494f6859a7998363e5d32655" target="_blank">www.ecpcp.eu/</a>
               <p>Tel Aviv Meeting<br> 
-              17-19 Mar 2023, Tel Aviv</p>
+              17-19 Mar 2023, Tel Aviv</p><br>
 
               <p><b>February 2023</b></p>
               <a href="https://ipa2023congress.org/index.php" target="_blank">www.ipa2023congress.org/</a>
               <p>30th International Pediatric Association Congress 2023 & 60th Annual Conference of The Indian Academy of Pediatrics<br> 
               “Quality Care for Every Child Everywhere”<br>
-              19th February – 23rd February 2023, Gujarat, India</p>
+              19th February – 23rd February 2023, Gujarat, India</p><br>
 
               <p><b>June 2019</b></p>
               <p>9. Europaediatrics Congress<br>
-              13-15 June 2019, Dublin, Ireland</p>
+              13-15 June 2019, Dublin, Ireland</p><br>
 
               <p><b>May 2015</b></p>
               <a href=""></a>
               <p>7th Europaediatrics Congress<br>
-              13-16 May 2015, FLORENCE, Italy</p>
+              13-16 May 2015, FLORENCE, Italy</p><br>
 
 
               <p> gelecek kongreler nelerdir???</p>
